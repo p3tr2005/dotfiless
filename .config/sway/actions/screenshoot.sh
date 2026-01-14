dedd8940646dbd5bb0fd14cbb7af427abff13b8d
petersahanaya <petersahanaya09@gmail.com>
2026-01-14 22:27:29 +0900

    feat: hyprland config.

--- .config/sway/actions/screenshoot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-slurp | grim -g - ~/Pictures/screenshoots/$(date +'%s_grim.png')
-
--- hdmi.sh
+++
@@ -1,2 +0,0 @@
-xrandr --output HDMI-A-2 --auto --same-as eDP-1
-#xrandr --output HDMI-1 --mode 1920x1080 --right-of eDP-1
